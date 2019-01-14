15fb5766e8284b7cd682b4f03c54b672df0c80f6
DaniilSmirnov <littlebigcrafting@gmail.com>
2019-01-14 18:46:05 +0300

    спинбокс подключен

--- main.py
+++ main.py
@@ -1,86 +1,78 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import networkx as nx
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName("gridLayout")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+
+class MainWidget(QWidget):
+
+    def __init__(self):
+        super(MainWidget, self).__init__()
+        font = QFont()
+        font.setPointSize(16)
+        self.initUI()
+
+    def initUI(self):
+
+        self.setGeometry(100, 100, 800, 600)
+        self.center()
+        self.setWindowTitle('S Plot')
+
+        self.gridLayout = QGridLayout()
+        self.setLayout(self.gridLayout)
+
+        self.label_2 = QLabel("Матрица смежности")
         self.label_2.setObjectName("label_2")
         self.gridLayout.addWidget(self.label_2, 0, 1, 1, 1)
-        self.line = QtWidgets.QFrame(self.centralwidget)
-        self.line.setFrameShape(QtWidgets.QFrame.VLine)
-        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line = QFrame()
+        self.line.setFrameShape(QFrame.VLine)
+        self.line.setFrameShadow(QFrame.Sunken)
         self.line.setObjectName("line")
         self.gridLayout.addWidget(self.line, 0, 2, 4, 1)
-        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget = QTableWidget()
         self.tableWidget.setObjectName("tableWidget")
-        self.tableWidget.setColumnCount(0)
-        self.tableWidget.setRowCount(0)
         self.gridLayout.addWidget(self.tableWidget, 1, 0, 1, 2)
-        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label = QLabel("Количество вершин")
         self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 2, 0, 1, 1)
-        self.spinBox = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox = QSpinBox()
         self.spinBox.setObjectName("spinBox")
         self.gridLayout.addWidget(self.spinBox, 2, 1, 1, 1)
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3 = QLabel("Путь:")
         self.label_3.setObjectName("label_3")
         self.gridLayout.addWidget(self.label_3, 3, 0, 1, 1)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
 
-        self.figure = Figure()
+        self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
-        self.gridLayout.addWidget(self.canvas, 0, 3, 1, 1)
-        MainWindow.setStatusBar(self.statusbar)
+        self.gridLayout.addWidget(self.canvas, 0, 3, 9, 9)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.spinBox.valueChanged.connect(self.matrix_draw)
 
-        self.plot1()
+        self.show()
+        #self.plot3()
 
-    def plot1(self):
-        self.figure.clf()
-
-        G = nx.Graph()
-        matrix = [(1, 2), (1, 3), (4, 2), (5, 1), (5, 2), (5, 3)]
-        G.add_edges_from(matrix)
-
-        nx.draw(G)
-
-        self.canvas.draw()
-
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label_2.setText(_translate("MainWindow", "TextLabel"))
-        self.label.setText(_translate("MainWindow", "TextLabel"))
-        self.label_3.setText(_translate("MainWindow", "TextLabel"))
+    def matrix_draw(self):
+        amount = int(self.spinBox.text())
+        i = 0
+        self.tableWidget.setRowCount(amount)
+        self.tableWidget.setColumnCount(amount)
+        headers = []
+        while i < amount:
+            headers.append(str(i))
+            i += 1
+        self.tableWidget.setHorizontalHeaderLabels(headers)
+        self.tableWidget.setVerticalHeaderLabels(headers)
 
     def plot(self):
+        self.figure.clf()
         G = nx.Graph()
         matrix = [(1, 2), (1, 3), (4, 2), (5, 1), (5, 2), (5, 3)]
         G.add_edges_from(matrix)
-
         nx.draw(G)
-        plt.show()
+        self.canvas.draw_idle()
 
     def center(self):
         qr = self.frameGeometry()
@@ -89,11 +81,12 @@ class Ui_MainWindow(object):
         self.move(qr.topLeft())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    app = QApplication(sys.argv)
+    app.aboutToQuit.connect(app.deleteLater)
+    app.setStyle(QStyleFactory.create("gtk"))
+    screen = MainWidget()
+    screen.show()
     sys.exit(app.exec_())
